9202be4da3db4461565e64b44bde661ffb8dee86
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-24 15:27:41 +0200

    Add Domain entities

---
+++ Users.Domain/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Users.Domain.Entities
+{
+    public class Address
+    {        
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
---
+++ Users.Domain/Entities/Film.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Users.Domain.Entities
+{
+    public class Film
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
