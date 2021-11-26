4b3fb68d2ead2c5ccbc733c01d57748df8531caf
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-26 12:34:22 +0200

    Add DTO models

---
+++ Users.WebApi/DTO/AddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Users.WebApi.DTO
+{
+    public class AddressDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
